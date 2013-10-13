37cc88dbd7389f0aaa264eb821166b77b74dce5a
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-10-13 02:42:12 +0200

    xt1060: Commonize configs
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,17 +28,5 @@
 
 LOCAL_PATH := device/motorola/xt1060
 
-# Use common resources
-TARGET_USES_MOTOROLA_MSM8960_COMMON_LIBLIGHT := true
-
-# NFC
-BOARD_HAVE_NFC := true
-
 #bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-
-#TWRP
-DEVICE_RESOLUTION := 720x1280
-
-# userdata size is double for maxx
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 12884901888
